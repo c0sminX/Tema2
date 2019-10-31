6eb534b674c74117e621c67bd059ebbb2f50eb22
cosmin <isaccosmin98@gmail.com>
2019-10-31 17:03:10 +0200

    Primul Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n,suma = 0;
+            int[] v = new int[50];
+            float media;
+            Console.Write("Introduceti numarul de valori: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Valoarea {0}: ", i+1);
+                v[i] = Convert.ToInt32(Console.ReadLine());
+                suma += v[i];
+            }
+
+            media = (float)suma / n;
+
+            Console.WriteLine("\n Media aritmetica a valorilor introduse este : {0} \n", media);
+        }
+    }
+}
